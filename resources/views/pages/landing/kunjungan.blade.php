8de9063087643494cc64fc9583628081d92923f8
imkrus <thecrusheda@gmail.com>
2020-11-12 14:14:58 +0700

    Fixing GetAPI from old Server to Local IP and Public IP

--- resources/views/pages/landing/kunjungan.blade.php
+++ resources/views/pages/landing/kunjungan.blade.php
@@ -239,7 +239,7 @@
                 </table>
             </div>
             <div class="col-lg-6">
-                asdsa
+                This Sistem is under contruction.
             </div>
         </div>
     </div>
@@ -252,6 +252,102 @@ $(document).ready(function() {
     $.ajax(
         {
             url: "http://192.168.1.3:8000/api/kunjungan/",
+            type: 'GET',
+            dataType: 'json', // added data type
+            success: function(res) {
+                // console.log(res.akomodasi.now)
+                var datenow = res.akomodasi.now;
+                var date = new Date(datenow);
+                // console.log(date);  // Wed Jan 01 2014 13:28:56 GMT-1000 (Hawaiian Standard Time)
+                $("#tglnow").append(date);
+                $("#tglyest").append(res.kunjungan.yest);
+                
+                // Data Rekap Kunjungan Pasien
+                    $.each(res.kunjungan.igd, function(index, el) {
+                        $("#kd_igd").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.anak, function(index, el) {
+                        $("#kd_anak").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.bedah, function(index, el) {
+                        $("#kd_bedah").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.gigi, function(index, el) {
+                        $("#kd_gigi").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.dalam, function(index, el) {
+                        $("#kd_dalam").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.orto, function(index, el) {
+                        $("#kd_ortopedi").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.jiwa, function(index, el) {
+                        $("#kd_jiwa").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.kulit, function(index, el) {
+                        $("#kd_kulit").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.tht, function(index, el) {
+                        $("#kd_tht").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.paru, function(index, el) {
+                        $("#kd_paru").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.syaraf, function(index, el) {
+                        $("#kd_syaraf").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.rehab, function(index, el) {
+                        $("#kd_rehab").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.keb, function(index, el) {
+                        $("#kd_kebidanan").append(el.jumlah);
+                    });
+                    $.each(res.kunjungan.mata, function(index, el) {
+                        $("#kd_mata").append(el.jumlah);
+                    });
+                // Data Rawat Inap Pasien
+                // console.log(res.akomodasi);
+                // res.akomodasi.forEach(function (item) {
+                //     $('#halo').append('<p>' + item + '</p>');
+                // })
+                    $.each(res.akomodasi.lt3, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#lt3").append(el.jumlah);
+                    });
+                    $.each(res.akomodasi.lt4, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#lt4").append(el.jumlah);
+                    });
+                    $.each(res.akomodasi.keb, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#keb").append(el.jumlah);
+                    });
+                    $.each(res.akomodasi.per, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#perin").append(el.jumlah);
+                    });
+                    $.each(res.akomodasi.iso, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#iso").append(el.jumlah);
+                    });
+                    $.each(res.akomodasi.icu, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#icu").append(el.jumlah);
+                    });
+                    $.each(res.akomodasi.total, function(index, el) {
+                        console.log(el.jumlah);
+                        $("#total").append(el.jumlah);
+                    });
+            }
+        }
+    );
+})
+</script>
+<script>   
+$(document).ready(function() { 
+    $.ajax(
+        {
+            url: "http://103.155.246.25:8000/api/kunjungan/",
             type: 'GET',
             dataType: 'json', // added data type
             success: function(res) {
