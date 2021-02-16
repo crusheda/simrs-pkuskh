b99d7e9a13bdb170655dcfeee0a34c162ede41c5
imkrus <thecrusheda@gmail.com>
2021-02-16 18:26:52 +0700

    change size of Frame Filemanager

--- resources/views/file-manager.blade.php
+++ resources/views/file-manager.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 
 <div class="card">
-    <iframe src="/token/jasnidh2qu8hnf3298r0fewniongisdng0f32hr0fiwdondnsajdn1283hr420hrwnoi" style="width: 100%; height: 550px; overflow: hidden; border: none;"></iframe>
+    <iframe src="/token/jasnidh2qu8hnf3298r0fewniongisdng0f32hr0fiwdondnsajdn1283hr420hrwnoi" style="width: 100%; height: 500px; overflow: hidden; border: none;"></iframe>
 </div>
 @endsection
