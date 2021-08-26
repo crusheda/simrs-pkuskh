b34b81761686f8f5bdf850364c9ca093aba69e4b
imkrus <thecrusheda@gmail.com>
2021-08-26 20:25:17 +0700

    Fix bug in Jquery and adding some button and redesigning blade view

--- resources/views/pages/it/pilar/index.blade.php
+++ resources/views/pages/it/pilar/index.blade.php
@@ -6,79 +6,90 @@
 <script src="{{ asset('js/jquery.dataTablesku.min.js') }}"></script>
 
 @can('imut_it')
-<div class="row">
-    <div class="col-md-3">
-        <div class="card" style="width: 100%">
-            <div class="card-header bg-dark text-white">
+<div class="card" style="width: 100%">
+    <div class="card-header bg-dark text-white">
 
-                <i class="fa-fw fas fa-plus nav-icon text-success">
+        <i class="fa-fw fas fa-database nav-icon text-info">
 
-                </i> Cari Nomer Rekam Medik
-                
-            </div>
-            <div class="card-body">
-                <label>Trans Akomodasi :</label>
-                <input type="number" name="rm" id="rm" max="99999999" class="form-control" placeholder="" autofocus required>
-                <br>
-                <div>
-                    <button class="btn btn-warning text-white pull-left" onclick="hapusTable()"><i class="fa-fw fas fa-trash nav-icon"></i> Clear</button>
-                    <button class="btn btn-success text-white pull-right" onclick="showRM()"><i class="fa-fw fas fa-check nav-icon"></i> Show</button>
+        </i> Hapus Kunjungan Pilar
+
+        <span class="pull-right badge badge-warning" style="margin-top:4px">
+            Akses IT
+        </span>
+        
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col">
+                <div class="form-inline pull-left">
+                    <div class="form-group mb-2">
+                        <label style="margin-top: -20px">No. Rekam Medik :</label>
+                    </div>
+                    <div class="form-group mx-sm-3 mb-2">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-warning text-white" onclick="hapusTable()"><i class="fa-fw fas fa-eraser nav-icon"></i></button>
+                            </div>
+                            <input type="number" name="rm" id="rm" max="99999999" class="form-control" placeholder="" autofocus required>
+                            <div class="input-group-append">
+                                <button class="btn btn-success text-white" id="btnshow" onclick="showRM()"><i class="fa-fw fas fa-search nav-icon"></i> Show</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="pull-right">
+                    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#readme" data-toggle="tooltip" data-placement="bottom" title="BACA SAYA"><i class="fa-fw fas fa-question nav-icon text-white"></i> Readme</button>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-9">
-        <div class="card" style="width: 100%">
-            <div class="card-header bg-dark text-white">
-
-                <i class="fa-fw fas fa-database nav-icon text-info">
-
-                </i> Detail Database Pilar
-
-                <span class="pull-right badge badge-warning" style="margin-top:4px">
-                    Akses IT
-                </span>
-                
-            </div>
-            <div class="card-body">
-                <div class="data-table-list">
-                    <div class="table-responsive">
-                        <table id="pilar" class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>RM</th>
-                                    <th>No.Pemeriksaan</th>
-                                    <th>Nama</th>
-                                    <th>Umur</th>
-                                    <th>Jenis Kelamin</th>
-                                    <th>Tgl Masuk</th>
-                                    <th>Tgl Keluar</th>
-                                    <th>#</th>
-                                </tr>
-                            </thead>
-                            <tbody id="tampil-tbody"></tbody>
-                        </table>
-                    </div>
-                </div>
+        <hr style="margin-top: -6px">
+        <div class="data-table-list">
+            <div class="table-responsive">
+                <small><kbd>Data Pasien</kbd> yang ditampilkan diurutkan berdasarkan <b>Tanggal Keluar</b> terakhir.</small><br><br>
+                <table id="pilar" class="table table-striped table-hover">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>RM</th>
+                            <th>No.Pemeriksaan</th>
+                            <th>Nama</th>
+                            <th>Umur</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Tgl Masuk</th>
+                            <th>Tgl Keluar</th>
+                            <th>#</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tampil-tbody"><tr><td colspan="8">Tidak ada data ditemukan.</td></tr></tbody>
+                </table>
             </div>
         </div>
     </div>
 </div>
 @endcan
+
+<div class="modal fade bd-example-modal-lg" id="readme" role="dialog" aria-labelledby="confirmFormLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h4 class="modal-title">
+                Baca Saya !
+            </h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                
+            </div>
+            <div class="modal-footer">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
-    $(document).ready( function () {
-        $('#pilar').DataTable(
-            {
-                paging: true,
-                searching: true,
-                dom: 'Bfrtip',
-                buttons: [
-                    'excel', 'pdf', 'print','colvis'
-                ],
-                order: [[ 1, "desc" ]],
-            }
-        );
-    } );
+    // $(document).ready( function () {
+    //     $('#pilar').DataTable();
+    // } );
 
     $("body").addClass('brand-minimized sidebar-minimized');
 
@@ -93,6 +104,7 @@
     });
 
     function showRM() {
+        $('#btnshow').prop('disabled', true); 
         var getrm = $("#rm").val();
         if (getrm.length == 4) {
             var rm = '0000'+getrm;
@@ -108,7 +120,7 @@
         }
         $("#rm").val(rm);
         $.ajax({
-            url: "http://103.155.246.25:8000/api/pilar/"+rm,
+            url: "http://192.168.1.3:8000/api/pilar/"+rm,
             type: 'GET',
             dataType: 'json', // added data type
             success: function(res) {
@@ -116,7 +128,20 @@
                 // $("#dateTable").empty();
                 res.show.forEach(item => {
                     var e = parseFloat(item.REG_KUNJUNGANPASIEN);
-                    $("#tampil-tbody").append(`<tr id="data${item.REG_KUNJUNGANPASIEN}"><td>${item.DAT_PASIEN}</td> <td>${item.REG_KUNJUNGANPASIEN}</td> <td>${item.NAMAPASIEN}</td> <td>${item.UMUR}</td> <td>${item.JNSKELAMIN}</td> <td>${item.TGL_REGISTRASI}</td> <td>${item.TGL_DISCHARGE}</td> <td><button class="btn btn-danger text-white" onclick="batalPeriksa(${e})"><i class="fa-fw fas fa-trash nav-icon"></i></button></td></tr>`);
+                    $("#tampil-tbody").append(`
+                        <tr id="data${item.REG_KUNJUNGANPASIEN}">
+                            <td>${item.DAT_PASIEN}</td> 
+                            <td>${item.REG_KUNJUNGANPASIEN}</td> 
+                            <td>${item.NAMAPASIEN}</td> <td>${item.UMUR}</td> 
+                            <td>${item.JNSKELAMIN}</td> <td>${item.TGL_REGISTRASI}</td> 
+                            <td>${item.TGL_DISCHARGE ? item.TGL_DISCHARGE : '<span class="badge badge-dark">NULL</span>'}</td> 
+                            <td>
+                                <button class="btn btn-danger text-white" onclick="batalPeriksa(${e})">
+                                    <i class="fa-fw fas fa-trash nav-icon"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    `);
                 });
                 // for (var i = 0, len = res.length; i < len; i++) {
                 //     $("#tampil-tbody").append(`res[i]`);
@@ -157,7 +182,7 @@
             /* Read more about isConfirmed, isDenied below */
             if (result.isConfirmed) {
                 $.ajax({
-                    url: "http://103.155.246.25:8000/api/pilar/batalperiksa/"+goal,
+                    url: "http://192.168.1.3:8000/api/pilar/batalperiksa/"+goal,
                     type: 'GET',
                     dataType: 'json', // added data type
                     success: function(res) {
@@ -169,12 +194,14 @@
                             icon: res.icon,
                             showConfirmButton:false,
                             showCancelButton:false,
-                            timer: 3000,
+                            timer: 5000,
                             timerProgressBar: true,
                             backdrop: `rgba(26,27,41,0.8)`,
                         });
                         // location.reload();
-                        window.setTimeout(function(){location.reload()},3000);
+                        if (res.icon == 'success') {
+                            window.setTimeout(function(){location.reload()},5000);
+                        }
                     },
                     // error: function(data){
                     //     Swal.fire({
@@ -216,6 +243,8 @@
     function hapusTable() {
         $("#rm").val("").empty();
         $("#tampil-tbody").empty();
+        $('#btnshow').prop('disabled', false); 
+        $("#tampil-tbody").append(`<tr><td colspan="8">Tidak ada data ditemukan.</td></tr>`);
     }
 </script>
 
