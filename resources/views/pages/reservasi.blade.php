2a1a6017813f50d941e701ea38289c91addf74ef
Yussuf Faisal <thecrusheda@gmail.com>
2022-06-03 14:30:34 +0700

    coba reservasi online IFRAME

---
+++ resources/views/pages/reservasi.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>RESERVASI</title>
+</head>
+<body>
+  <style>
+    iframe {
+        display:block;
+        width:100%;
+        height:91vh;
+    }
+  </style>
+  <iframe src="http://192.168.1.3/m" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
