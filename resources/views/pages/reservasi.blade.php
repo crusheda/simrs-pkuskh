e017e8450474c951b3d95593c27c790a0df4adf3
Yussuf Faisal <thecrusheda@gmail.com>
2022-06-04 14:46:50 +0700

    cek reservasi fullscreen

--- resources/views/pages/reservasi.blade.php
+++ resources/views/pages/reservasi.blade.php
@@ -7,13 +7,36 @@
   <title>RESERVASI</title>
 </head>
 <body>
-  <style>
-    iframe {
-        display:block;
-        width:100%;
-        height:91vh;
+  <style type="text/css">
+    html {
+        overflow: auto;
     }
-  </style>
-  <iframe src="http://192.168.1.3/m" frameborder="0" allowfullscreen></iframe>
+      
+    html,
+    body,
+    div,
+    iframe {
+        margin: 0px;
+        padding: 0px;
+        height: 100%;
+        border: none;
+    }
+      
+    iframe {
+        display: block;
+        width: 100%;
+        border: none;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
+</style>
+  <iframe src="http://192.168.1.3/m"
+    frameborder="0" 
+    marginheight="0" 
+    marginwidth="0" 
+    width="100%" 
+    height="100%" 
+    scrolling="auto">
+  </iframe>
 </body>
 </html>
