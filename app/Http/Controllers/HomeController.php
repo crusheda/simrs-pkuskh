6895e044e11844d4eb0964c535e2fcecc132c97e
Faisal <thecrusheda@gmail.com>
2022-11-02 13:34:55 +0700

    uji coba bridging bpjs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ use App\Models\tdkperawat;
 use App\Models\user;
 use App\Models\logs;
 use Carbon\Carbon;
+use AamDsam\Bpjs\VClaim;
 use Auth;
 
 class HomeController extends Controller
@@ -85,11 +86,16 @@ class HomeController extends Controller
                 ->first();  
                 
         $showlog = logs::where('user_id', $id)->where('log_type', '=', 'login')->select('log_date')->orderBy('log_date', 'DESC')->get();
+
+        $referensi = new VClaim\Peserta($this->vclaim_conf());
+        // print_r($referensi);
+        // die();
         
         $data = [
             'user' => $user,
             'foto' => $foto,
             'showlog' => $showlog,
+            // 'referensi' => $referensi->propinsi(),
         ];
 
         return view('pages.new.dashboard')->with('list', $data);
@@ -98,5 +104,16 @@ class HomeController extends Controller
     public function fileManager()
     {
         return view('pages.new.administrasi.filemanajer');
+    }
+
+    function vclaim_conf(){
+        $config = [
+            'koders' => env('BPJS_KODERS'),
+            'cons_id' => env('BPJS_CONSID'),
+            'secret_key' => env('BPJS_SCREET_KEY'),
+            'base_url' => env('BPJS_BASE_URL'),
+            'service_name' => env('BPJS_SERVICE_NAME'),
+        ];
+        return $config;
     }
 }
