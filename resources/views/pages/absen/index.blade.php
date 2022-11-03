b270e962a2ce9de309a1915a1599ceff9c0ec268
Yussuf Faisal <thecrusheda@gmail.com>
2022-11-03 23:03:26 +0700

    Test Webcam

--- resources/views/pages/absen/index.blade.php
+++ resources/views/pages/absen/index.blade.php
@@ -61,6 +61,8 @@
   </script>
   <!-- Custom notification for demo -->
   <!-- beautify ignore:end -->
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 
 </head>
 
@@ -119,6 +121,25 @@
       <!-- /Two Steps Verification -->
     </div>
   </div>
+  <br><br>
+  <form method="POST" action="{{ route('antrol.display') }}">
+    @csrf
+    <div class="row">
+        <div class="col-md-6">
+            <div id="my_camera"></div>
+            <br/>
+            <input type=button value="Take Snapshot" onClick="take_snapshot()">
+            <input type="hidden" name="image" class="image-tag">
+        </div>
+        <div class="col-md-6">
+            <div id="results">Your captured image will appear here...</div>
+        </div>
+        <div class="col-md-12 text-center">
+            <br/>
+            <button class="btn btn-success">Submit</button>
+        </div>
+    </div>
+  </form>
 
   <!-- / Content -->
   
@@ -147,7 +168,21 @@
   
   <script>
     $(document).ready( function () {
+      Webcam.set({
+          width: 490,
+          height: 350,
+          image_format: 'jpeg',
+          jpeg_quality: 90
+      });
+      Webcam.attach( '#my_camera' );
     })
+    
+    function take_snapshot() {
+        Webcam.snap( function(data_uri) {
+            $(".image-tag").val(data_uri);
+            document.getElementById('results').innerHTML = '<img src="'+data_uri+'"/>';
+        } );
+    }
 
     function absen() {
       var params = $("#ip").val();
